856431f661276cbb9f0a6d4b3bea512d48d8903c
mathewh22 <mathewhuang22@gmail.com>
2023-02-07 13:20:25 -0600

    Create getMetaBy.py

---
+++ getMetaBy.py
@@ -0,0 +1,9 @@
+import scipy as sp
+import numpy as np
+
+def getMetaBy(Location, Activity, Viewer, Partner):
+    location = Location.split(",")
+    activity = Activity.split(",")
+    viewer = Viewer.split(",")
+    partner = Partner.split(",")
+    
